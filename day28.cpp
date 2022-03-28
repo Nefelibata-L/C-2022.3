8824fabeeac5a16f3a28b588dd9a483a29a00463
bajihuo <3405235455@qq.com>
2022-03-28 19:02:10 +0800

    day28

---
+++ day28.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//转义字符
+int main() {
+	printf("%s\n", "abcd??)");//三字母词:??)将转换成]  vs2019中不支持
+	printf("%s\n", "abcd\??)");
+	printf("%c\n",'\'');
+	printf("%s\n","\"");
+	printf("%s\n", "\\n");
+	printf("\a");
+	printf("%c\n",'\123');//3位数八进制数字123转换为十进制数字83所对应的ASCLL码
+	printf("%c\n", '\x12');//2位数十六进制数字12转换为十进制数字18所对应的ASCLL码
+	
+	return 0;
+}
+
